9c4579fdff3c4695da26bf86478a788b47305349
januwA <ajanuw1641845087@sina.com>
2020-01-07 21:14:56 +0800

    优化方法

--- PrintHelp.pm
+++ PrintHelp.pm
@@ -32,17 +32,17 @@
 
 # Demo
 #
-# Usage:  atree [dir=./] [options]
-#         atree ./lib
-#         atree ./lib --depath 1
-#         or
-#         atree ./lib -d 1
-#         atree -i ~/.atree -d 3
+# Usage: atree [dir=./] [options]
+#        atree ./lib
+#        atree ./lib --depath 1
+#        or
+#        atree ./lib -d 1
+#        atree -i ~/.atree -d 3
 
 # Options:
-#   -h, --help    帮助文档.
-#   -i, --ignores 无视目录配置文件路径. (default: ./atree)
 #   -d, --depath  查询目录深度. (default: 0 无限.)
+#   -i, --ignores 无视目录配置文件路径. (default: ./atree)
+#   -h, --help    帮助文档.
 
 package PrintHelp;
 require Exporter;
@@ -62,44 +62,61 @@ sub new {
       usages   => shift,
       options  => shift,
    };
-
-   my $opts = $self->{options};
-   my $usages = $self->{usages};
-
-   my $maxLen = 0;
-   for(keys %{$opts}) {
-      $maxLen = length($_) if(length($_) > $maxLen);
-   }
+   bless $self, $class;
 
    my $help = "";
+   $help .= $self->setUsages();
+   $help .= $self->setOptions();
+   say encode('utf-8', $help);
+   return $self;
+}
 
+# 获取options中命令的最大长度
+sub getMaxLen {
+   my( $self ) = @_;
+   my $maxLen = 0;
+   for(keys %{$self->{options}}) {
+      $maxLen = length($_) if(length($_) > $maxLen);
+   }
+   return $maxLen;
+}
+
+# 设置Usages
+sub setUsages {
+   my( $self ) = @_;
+   my $help = "";
+   my $usages = $self->{usages};
    if(defined($usages)){
       my $usagesLen = @{$usages};
       for(my $i=0; $i < $usagesLen; $i++){
-         $help .= "Usage:  " if($i == 0);
-         $help .= " " x 8 if($i != 0);
+         $help .= ($i == 0) ? "Usage: " : " " x 7;
          $help .= "$usages->[$i]\n";
       }
       $help .= "\n";
    }
+   return $help;
+}
 
-
-   $help .= "Options:\n";
-   for(keys %{$opts}) {
-      my $keyLen = length($_);
-      $help .= "  ";
-      $help .= "-$opts->{$_}{\"alias\"}, " if(defined($opts->{$_}{"alias"}));
-      $help .= "--$_";
-      $help .= " " x ($maxLen - $keyLen) if($keyLen < $maxLen);
-      $help .= "\t$opts->{$_}{\"msg\"}" if(defined($opts->{$_}{"msg"}));
-      $help .= " (default: $opts->{$_}{\"default\"})" if(defined($opts->{$_}{"default"}));
-      $help .= "\n";
-   };
-
-   say encode('utf-8', $help);
-   
-   bless $self, $class;
-   return $self;
+# 设置options
+sub setOptions {
+   my( $self ) = @_;
+   my $help = "";
+   my $opts = $self->{options};
+   if(defined($opts)){
+      my $maxLen = $self->getMaxLen();
+      $help .= "Options:\n";
+      for(keys %{$opts}) {
+        my $keyLen = length($_);
+        $help .= "  ";
+        $help .= "-$opts->{$_}{\"alias\"}, " if(defined($opts->{$_}{"alias"}));
+        $help .= "--$_";
+        $help .= " " x ($maxLen - $keyLen) if($keyLen < $maxLen);
+        $help .= "\t$opts->{$_}{\"msg\"}" if(defined($opts->{$_}{"msg"}));
+        $help .= " (default: $opts->{$_}{\"default\"})" if(defined($opts->{$_}{"default"}));
+        $help .= "\n";
+      };
+   }
+   return $help;
 }
 
 our @ISA = qw(Exporter); # 继承Exporter
